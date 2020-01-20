3e86ae0db93ae5da3c5485aa3a5bf611bd1d8490
Omur Turan <omur@unumotors.com>
2020-01-21 01:00:30 +0300

    Make linter happy

--- src/views/credits.vue
+++ src/views/credits.vue
@@ -21,14 +21,19 @@ export default {
 </script>
 <template>
   <div>
-    <progress-bar :slots="walkpathInProgress.composition">
-    </progress-bar>
+    <progress-bar :slots="walkpathInProgress.composition"> </progress-bar>
     <div class="duration">
       {{ walkpathInProgress.duration | secondsToMinutes }}min of
       {{ walkpathInProgress.duration | secondsToMinutes }}min
     </div>
     <div>
-      END PAGE Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress.
+      END PAGE Turnip greens yarrow ricebean rutabaga endive cauliflower sea
+      lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage
+      asparagus winter purslane kale. Celery potato scallion desert raisin
+      horseradish spinach carrot soko. Lotus root water spinach fennel kombu
+      maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea
+      gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard
+      wakame kohlrabi beetroot carrot watercress.
     </div>
   </div>
 </template>
